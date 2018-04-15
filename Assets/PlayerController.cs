78d5107b32fff06cc30e635401aff9ca14fa4354
aosar <ariannaosar@gmail.com>
2018-04-15 11:35:35 -0500

    Added crosshair and placeholder text, and some dialog on the back end

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
     public float speed;
     private Rigidbody rb;
+    private string[] textSequence;
+    private int dialogIndex;
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
-	}
+        textSequence = new string[] { "Hey! I need your help with something.", "I was going for a walk with my cat Bella and suddenly she wasn't with me", "Can you help me find her?" };
+        dialogIndex = 0;
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
